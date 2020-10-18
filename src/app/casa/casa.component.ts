4f0058feadf67ea487f48d773cca25d1300fb6da
Matteo Borghesi <ma.borghe@gmail.com>
2020-10-18 20:26:25 +0200

    Improve home page

--- src/app/casa/casa.component.ts
+++ src/app/casa/casa.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-casa',
@@ -6,5 +6,6 @@ import {Component} from '@angular/core';
   styleUrls: ['./casa.component.css']
 })
 export class CasaComponent {
+
 
 }
