f674ca4e1146fb8003d90d874da290e3f803b10b
Matteo Borghesi <ma.borghe@gmail.com>
2020-10-20 19:11:56 +0200

    Add content to CasaComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,10 @@ import { NgModule } from '@angular/core';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatButtonModule} from '@angular/material/button';
 
+import {AppRoutingModule} from './app-routing.module';
 import { AppComponent } from './app.component';
-import {RouterModule} from '@angular/router';
 import {PageNotFoundComponent} from './app.helper';
 import {HomeComponent} from './home/home.component';
-import {AppRoutingModule} from './app-routing.module';
 import {CasaComponent} from './casa/casa.component';
 
 @NgModule({
