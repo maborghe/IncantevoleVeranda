b42da470c9d477ffa5257b41f2dcd0724f32e809
Matteo Borghesi <ma.borghesi@goreply.io>
2025-02-16 10:42:26 +0100

    Add foto component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import {Routes} from '@angular/router';
 import {PageNotFoundComponent} from './app.helper';
 import {HomeComponent} from './home/home.component';
 import {CasaComponent} from './casa/casa.component';
+import {FotoComponent} from './foto/foto.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'casa', component: CasaComponent },
+  { path: 'foto', component: FotoComponent },
   { path: '**', component: PageNotFoundComponent }
 ];
---
+++ src/app/foto/foto.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-foto',
+  templateUrl: './foto.component.html',
+  styleUrls: ['./foto.component.css'],
+  standalone: true,
+})
+export class FotoComponent {
+
+
+}
