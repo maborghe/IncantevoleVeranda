0a15c0412d211b70a119001e2acc058a0ea5eb34
Matteo Borghesi <ma.borghesi@goreply.io>
2025-02-16 10:13:54 +0100

    Add link to casa in menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import {Component} from '@angular/core';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatButtonModule} from '@angular/material/button';
 import {RouterOutlet, RouterLink} from '@angular/router';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [MatToolbarModule, RouterOutlet, RouterLink],
-  //imports: [MatToolbarModule, RouterOutlet],
+  imports: [MatToolbarModule, MatButtonModule, MatMenuModule, RouterOutlet, RouterLink ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
