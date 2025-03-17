e71f0fddf149f0fe6079359e2a36d5c08396e5b9
Matteo Borghesi <ma.borghesi@goreply.io>
2025-03-17 19:50:21 +0100

    Add all beaches

--- src/app/spiagge/spiagge.component.ts
+++ src/app/spiagge/spiagge.component.ts
@@ -10,12 +10,31 @@ import { SlideshowImage } from '../slideshow/slideshow-image';
   standalone: true,
 })
 export class SpiaggeComponent {
-  vicinanzeItems = {
+  nelleVicinanze = {
     title: "Nelle vicinanze",
     images: [
       new SlideshowImage('assets/spiagge/tre-monti.jpg', 'Tre monti', '750m', '10min 🚶'),
       new SlideshowImage('assets/spiagge/battistoni.jpg', 'Cala Battistoni', '850m', '12min 🚗'),
       new SlideshowImage('assets/spiagge/cala-bitta.jpg', 'Cala Bitta', '3km', '6min 🚶'),
+    ]
+  }
+
+  dogBeaches = {
+    title: "Dog beaches",
+    images: [
+      new SlideshowImage('assets/spiagge/lapitrizza.png', 'La Pitrizza', '6km', '8min 🚗'),
+      new SlideshowImage('assets/spiagge/lupostu.jpg', 'Lu Postu', '8km', '10min 🚗'),
+      new SlideshowImage('assets/spiagge/sciumara.png', 'La Sciumara', '12km', '15min 🚗'),
+    ]
+  }
+
+  costaSmeralda = {
+    title: "Costa Smeralda",
+    images: [
+      new SlideshowImage('assets/spiagge/pevero.jpg', 'Pevero', '9km', '13min 🚗'),
+      new SlideshowImage('assets/spiagge/capriccioli.jpg', 'Capriccioli', '15km', '19min 🚗'),
+      new SlideshowImage('assets/spiagge/principe.jpg', 'Spiaggia del principe', '16km', '22min 🚗'),
+      new SlideshowImage('assets/spiagge/romazzino.jpg', 'Romazzino', '16km', '22min 🚗'),
     ]
   }
 
